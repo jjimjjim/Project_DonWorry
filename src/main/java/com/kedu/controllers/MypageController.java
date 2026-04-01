a0ab5e095ffd2e6526cf57432a756556d0e7268a
Seol <chlo01233@gmail.com>
2026-04-01 09:24:03 +0900

    account, withdraw re-created

--- src/main/java/com/kedu/controllers/MypageController.java
+++ src/main/java/com/kedu/controllers/MypageController.java
@@ -4,20 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("mypage")
+@RequestMapping("/mypage")
 public class MypageController {
 	
-	@RequestMapping("toMypage")
+	@RequestMapping("/toMypage")
 	public String toMypage() {
 		return "mypage/mypage";
 	}
 	
-	@RequestMapping("toAccount")
+	@RequestMapping("/toAccount")
 	public String toAccount() {
 		return "mypage/account";
 	}
 	
-	@RequestMapping("toWithdraw")
+	@RequestMapping("/toWithdraw")
 	public String toWithdraw() {
 		return "mypage/withdraw";
 	}
